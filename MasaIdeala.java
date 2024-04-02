7195040f33211c31a08ad263fcb21c642f1a06f7
Florin Angeru <florinangeru@gmail.com>
2024-04-02 20:29:24 +0300

    adaugare fisier MasaIdeala.class

--- MasaIdeala.java
+++ MasaIdeala.java
@@ -2,23 +2,23 @@ import javax.swing.JOptionPane;
 
 public class MasaIdeala {
     public static void main(String[] args) {
-        // Solicită utilizatorului să introducă înălțimea și vârsta prin intermediul unor ferestre de dialog
-        String inaltimeStr = JOptionPane.showInputDialog(null, "Introduceți înălțimea în centimetri:");
-        String varstaStr = JOptionPane.showInputDialog(null, "Introduceți vârsta în ani:");
+        // Solicitam introducerea inaltimii si varsta prin intermediul ferestrelor de dialog
+        String inaltimeStr = JOptionPane.showInputDialog(null, "Introduceti inaltimea in centimetri:");
+        String varstaStr = JOptionPane.showInputDialog(null, "Introduceti varsta in ani:");
 
-        // Convertim valorile introduse de utilizator din String în float
+        // Convertim valorile introduse de la tastatura din String in float
         float inaltime = Float.parseFloat(inaltimeStr);
         float varsta = Float.parseFloat(varstaStr);
 
-        // Calculăm masa ideală folosind formulele specificate
+        // Calculam masa ideala folosind formulele specificate in cerinta
         float masa = 50 + 0.75f * (inaltime - 150) + 0.25f * (varsta - 20);
         float masaIdealaBarbati = masa;
         float masaIdealaFemei = masa * 0.9f;
 
-        // Formatarea rezultatului la două zecimale pentru ambii și combinarea lor într-un singur mesaj
-        String rezultat = String.format("Masa ideală pentru bărbați: %.2f kg\nMasa ideală pentru femei: %.2f kg", masaIdealaBarbati, masaIdealaFemei);
+        // Formatam rezultatul la doua zecimale pentru ambele categorii si le combinam intr-un singur mesaj
+        String rezultat = String.format("Masa ideala pentru barbati: %.2f kg\n Masa ideala pentru femei: %.2f kg", masaIdealaBarbati, masaIdealaFemei);
 
-        // Afișăm rezultatul combinat printr-o fereastră de dialog
+        // Afisam rezultatul intr-o singura fereastra de dialog
         JOptionPane.showMessageDialog(null, rezultat);
     }
 }
